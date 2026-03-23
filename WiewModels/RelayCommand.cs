171911d9524531f43a55573eef2527ea963bf866
rimoondust <101406524+AntoSubo@users.noreply.github.com>
2026-03-23 10:46:01 +0700

    Update RelayCommand.cs

--- WiewModels/RelayCommand.cs
+++ WiewModels/RelayCommand.cs
@@ -1,12 +1,47 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EditWave.WiewModels
 {
-    internal class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            if (_execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return _canExecute(parameter);
+            }
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
     }
 }
